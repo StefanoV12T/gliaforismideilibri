b26269c7ed6abacf5ad5b9408dbf61141a0a7d70
Stefano <stefanociangola@gmail.com>
2023-10-29 19:45:32 +0100

    inizializzazione sito con bootstrap e navbar

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gli Aforismi dei Libri</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body>
+    {{$slot}}
+  </body>
+</html>
