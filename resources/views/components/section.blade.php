d5222c96ff63510a02dcbce76f451594f8054c54
Stefano Ciangola <stefanociangola@gmail.com>
2024-02-02 15:04:01 +0100

    vista welcome carica le recensioni dal database

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,24 +1,58 @@
-<div class="container my-5">
+<div class="container ">
     <div class="row ms-5">
-        <div class="col-12 col-lg-5"><img src="{{asset('img\OIP (1).jfif')}}" alt="" class="img-fluid card v-100">
+        <div class="col-12 col-lg-5 myHeight my-5"><img src="{{asset('img\OIP (1).jfif')}}" alt="" class="img-fluid card h-100">
         <h3 class="text-white">Titolo</h3>
-        <a href="{{route('show-review')}}">leggi recensine</a>
+        <a href="{{route('show-review')}}">leggi recensione</a>
         <p  class="text-white">questa è la <strong>recensione</strong></p>
         </div>
-        <div class="col-12 col-lg-5"><img src="{{asset('img/download.jfif')}}" alt="" class="img-fluid card v-100"><h3 class="text-white">Titolo</h3>
+        <div class="col-12 col-lg-5 myHeight my-5"><img src="{{asset('img/download.jfif')}}" alt="" class="img-fluid card h-100"><h3 class="text-white">Titolo</h3>
+        <a href="{{route('show-review')}}">leggi recensione</a>
         <p  class="text-white">questa è la <strong>recensione</strong></p>
-    
         </div>
     </div>
 
-    <div class="row ms-5">
-        <div class="col-12 col-lg-5"><img src="{{asset('img\OIP.jfif')}}" alt="" class="img-fluid card v-100"><h3 class="text-white">Titolo</h3>
-        <p  class="text-white">questa è la <strong>recensione</strong></p>
+   
+
+    {{-- <div class="container">
+        <div class="row">
+            <div class="col-12 overlay my-5 card">
+                <h2 class="h2 m-5 fw-bold neonText2">{{__('ui.allAnnouncements')}}</h2>
+                <div class="row">
+                    @foreach ($reviews as $review)
+                    <div class="col-12 col-md-6 col-lg-4 my-4">
+                        <div class="card mx-auto shadow-mrk h-100" data-aos="zoom-in"  data-aos-duration="300" style="width: 18rem;">
+                            <img src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(400,300) : 'https://picsum.photos/400/300'}}" class="card-img-top" alt="...">
+                            <div class="card-body " >
+                              <h5 class="card-title title-dimension overflow-hidden" >{{$announcement->title}}</h5>
+                                
+                                <p class="card-text">{{__('ui.price')}}: €{{$announcement->price}}</p>
+                                <a href="{{route('announcement.show',compact('announcement'))}}" class="btn w-100 btn-warning">{{__('ui.details')}}</a>
+                                <a href="{{route('categoryShow',['category'=>$announcement->category])}}" class="btn w-100 my-2 btn-warning">{{__('ui.category')}}: 
+                                  
+                                @switch(session('locale'))
+                                @case('en')
+                                {{$announcement->category->English}}
+                                @break
+                                @case('es')
+                                {{$announcement->category->Spanish}}
+                                @break
+                                
+                                @default
+                                {{$announcement->category->name}}
+                                @endswitch
+                    
+
+                                </a>
+                                <p class="card-footer bg-white">{{__('ui.publishedOn')}}: {{$announcement->created_at->format('d/m/y')}} <br>{{__('ui.author')}}: {{$announcement->user->name}}</p>
+                            </div>
+                          </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <div class="col-12 col-lg-5"><img src="{{asset('img\pila.jfif')}}" alt="" class="img-fluid card v-100"><h3 class="text-white">Titolo</h3>
-        <p  class="text-white">questa è la <strong>recensione</strong></p>
-    
-        </div>
-    </div>
+    </div> --}}
+
+
 
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,30 @@
 <x-main>
     <x-section/>
-    <x-section/>
-    <x-section/>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 overlay my-5 card">
+                <h2 class="h2 m-5 fw-bold neonText2">Recensioni</h2>
+                <div class="row">
+                    @foreach ($reviews as $review)
+                    <div class="col-12 col-md-6 col-lg-4 my-4">
+                        <div class="card mx-auto shadow-mrk h-100" data-aos="zoom-in"  data-aos-duration="300" style="width: 18rem;">
+                            <img src="
+                            {{-- {{!$review->images()->get()->isEmpty() ? $review->images()->first()->getUrl(400,300) :  --}}
+                            https://picsum.photos/400/300
+                        {{-- }} --}}
+                        " class="card-img-top" alt="...">
+                            <div class="card-body " >
+                              <h5 class="card-title title-dimension overflow-hidden" >{{$review->title}}</h5>
+                                <a href="{{route('show-review',compact('review'))}}" class="btn w-100 btn-warning">Leggi recensione</a>
+                                {{-- <a href="{{route('categoryShow',['category'=>$review->category])}}" class="btn w-100 my-2 btn-warning">{{__('ui.category')}}:</a> --}}
+                                <p class="card-footer bg-white">Recensito il: {{$review->created_at->format('d/m/y')}} <br>Autore: {{$review->user->name}}</p>
+                            </div>
+                          </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </x-main>
