e133e860c45952cadd8c0491e6e37a60a739341c
Stefano <stefanociangola@gmail.com>
2024-02-10 01:35:50 +0100

    cookies in italiano (necessari per tutti)

--- lang/vendor/cookie-consent/en/texts.php
+++ lang/vendor/cookie-consent/en/texts.php
@@ -8,6 +8,9 @@
 // ];
 
 return [
-    'message' => 'Questo sito utilizza i cookies solo per offrire un\'esperienza migliore all\'utente. I dati immessi non verranno condivisi a terzi.' ,
-    'agree' => 'Consenti i cookies obbligatori',
+    'message' => 'Questo sito utilizza i cookie per garantire le funzionalità del sito e mantenere i nostri servizi, migliorare la tua esperienza online, analizzare le statistiche e visualizzare annunci o contenuti personalizzati. Cliccando su "Consenti i cookie essenziali", verranno utilizzati solo i cookie necessari per fornirti i servizi.',
+    'agree' => 'Consenti i cookies essenziali',
 ];
+
+
+// Questo sito utilizza i cookie per garantire le funzionalità del sito e mantenere i nostri servizi, migliorare la tua esperienza online, analizzare le statistiche e visualizzare annunci o contenuti personalizzati. Cliccando su "Accetta tutti i cookie", acconsenti all'uso dei cookie da parte nostra, dei nostri partner e/o di terzi (se presenti). Cliccando su "Accetta solo i cookie essenziali", verranno utilizzati solo i cookie necessari per fornirti i servizi. Puoi gestire le impostazioni dei cookie cliccando su "Impostazioni dei cookie". Per ulteriori informazioni o per modificare le impostazioni dei cookie in qualsiasi momento, visita la nostra Informativa sui Informativa sui Cookie e Norme sulla Privacy.
