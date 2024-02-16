bafee00503802355a40fe77e9cf2655e6d292f81
Stefano <stefanociangola@gmail.com>
2024-02-16 22:57:17 +0100

    viste sistemate

--- resources/views/categories/searched.blade.php
+++ resources/views/categories/searched.blade.php
@@ -2,24 +2,71 @@
     <div class="container overlay card mt-5">
         <div class="row">
             <div class="col-12 mt-3">
-                <h2 class="h2 my-5 pt-3 fw-bold text-center neonText2">{{$category->name}}</h2>
+                <h2 class="h2 my-5 pt-3 fw-bold text-white text-center neonText2">{{$category->name}}</h2>
                 <div class="row">
                     @forelse  ( $category->reviews as $review)
                    {{-- @if ($announcement->is_accepted) --}}
-                   <div class="col-12 col-md-6 col-lg-4 my-4">
-                    <div class="card shadow-mrk mx-auto h-100"  data-aos="zoom-in-down" data-aos-duration="300" style="width: 18rem;">
-                        {{-- <img src="{{!$review->images()->get()->isEmpty() ? $review->images()->first()->getUrl(400,300) : 'https://picsum.photos/400/300'}}" class="card-img-top" alt="..."> --}}
-                        <div class="card-body">
-                          <h5 class="card-title">{{$review->title}}</h5>
-                          {{-- <p class="card-text">{{$announcement->body}}</p> --}}
-                         
-                          <a href="{{route('show-review',compact('review'))}}" class="btn btn-warning mb-2 w-100">Leggi recensione</a>
-                          
-                          <p class="card-footer bg-white">Pubblicato il: {{$review->created_at->format('d/m/y')}} <br>
-                            da: {{$review->user->name ?? ''}}</p>
-                        </div>
-                      </div>
-                    </div> 
+                   <div class="col-12 col-md-6 col-xl-4 my-4">
+                    <div class="flip-card-container" >
+                          <div class="flip-card">      
+                            <div class="card-front">
+                              <figure>
+                                <div class="img-bg"></div>
+                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake">
+                                <figcaption><h5 class="card-title p-1 title-dimension overflow-hidden" >{{$review->title}}</h5>
+                                  <span>di</span>
+                                  <h6 class="d-inline">{{$review->author}}</h6></figcaption>              
+                              </figure>
+                              <ul class="ul px-0">
+                                <li class="li"> <p class="card-footer">Recensito il: {{$review->created_at->format('d/m/y')}} </p></li>
+                                <li class="li"><p class="card-footer">Da: {{$review->user->name}}</p></li>
+                                <li class="li">Categorie: </li>
+                                @foreach ($review->categories as $category)
+                                                 <li class="li">{{$category->name}}</li>                              
+                                                 @endforeach 
+                              </ul>
+                            </div>
+                        
+                            <div class="card-back">
+                              <figure>
+                                <div class="img-bg"></div>
+                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake">
+                              </figure>
+                                  <a href="{{route('show-review', $review)}}" class="btn button  t-w">Leggi recensione</a>      
+                              <div class="design-container">
+                                <span class="design design--1"></span>
+                                <span class="design design--2"></span>
+                                <span class="design design--3"></span>
+                                <span class="design design--4"></span>
+                                <span class="design design--5"></span>
+                                <span class="design design--6"></span>
+                                <span class="design design--7"></span>
+                                <span class="design design--8"></span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>    
+
+                      {{-- <div class="card mx-auto shadow-mrk h-100" data-aos="zoom-in"  data-aos-duration="300" style="width: 18rem;">
+                          <img src="
+                          {{-- {{!$review->images()->get()->isEmpty() ? $review->images()->first()->getUrl(400,300) :  --}}
+                          {{-- https://picsum.photos/400/300 --}}
+                      {{-- }} --}}
+                      {{--" class="card-img-top" alt="...">
+                          <div class="card-body " >
+                            <h5 class="card-title p-1 title-dimension overflow-hidden" >{{$review->title}}</h5>
+                            <span>di</span>
+                            <h6 class="d-inline">{{$review->author}}</h6>
+                              <a href="{{route('show-review', $review)}}" class="btn w-100 btn-warning">Leggi recensione</a>
+                              {{-- <a href="{{route('categoryShow',['category'=>$review->category])}}" class="btn w-100 my-2 btn-warning">{{__('ui.category')}}:</a> --}}
+                             {{-- <p class="card-footer bg-white">Recensito il: {{$review->created_at->format('d/m/y')}} <br>Da: {{$review->user->name}}</p>
+                             <h6>Categorie: </h6>
+                             @foreach ($review->categories as $category)
+                             <a class="btn  h-10 btn-warning p-1" href="{{route('categories-searched', $category)}}">{{$category->name}}</a>                              
+                             @endforeach 
+                          </div>
+                        </div> --}}
+                  </div>
                    {{-- @endif --}}
                    @empty
                    
@@ -31,6 +78,7 @@
                     
                     
                     <div style="height: 150px"></div>
+                    {{$reviews->appends(Request::except('page'))->links()}}
                     @endforelse
                 </div>
             </div>
--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
           <div class="col-12 overlay my-5 card">
-            <h2 class="h2 m-5 fw-bold neonText2 text-white">Tutte l recensioni</h2>
+            <h2 class="h2 m-5 fw-bold neonText2 text-white">Tutte le recensioni</h2>
               <div class="row">
                 <x-section :reviews="$reviews"/>   
                 {{$reviews->appends(Request::except('page'))->links()}}
