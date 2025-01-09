62d2932afe8ca8ffeaf4e8d7e70ed3f03b41b0f7
Stefano Ciangola <stefanociangola@gmail.com>
2025-01-10 00:45:23 +0100

    build con footer sempre in fondo alla pagina

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -23,7 +23,7 @@
     {{-- @include('cookie-consent::index')   --}}
     <footer class="footer">
       <x-footer/>
-      {{-- <x-banner-cookies/> --}}
+      <x-banner-cookies/>
     </footer>
   </body>
 </html>
